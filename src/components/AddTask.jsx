9e0f12e58b355f8951e04d59a86d479956f17957
OriMelamed <ori.melamed@gmail.com>
2024-03-03 15:10:05 +0200

    Add useForm hook

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,28 +1,35 @@
 import { useState, useRef } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function AddTask({ addTask }) {
     const [taskItem, setTaskItem] = useState('');
     const taskRef = useRef()
 
+    const { register, handleSubmit, formState: { errors }, reset } = useForm()
 
-    const handleClick = () => {
-        addTask(taskRef.current.value)
-        taskRef.current.value = ''
+    const handleClick = (data) => {
+        addTask(data.task)
+        reset()
     }
 
     return (
         <div>
-            <input className=" bg-slate-100 rounded-md p-4 m-4"
-                type="text"
-                ref={taskRef}
-                placeholder="Create a new todo"
-            />
-            <button
-                className='  bg-blue-600 font-bold px-6 py-2 rounded-full'
-                onClick={handleClick}
-            >
-                Add Task
-            </button>
+            <form onSubmit={handleSubmit(handleClick)}>
+                <div>
+                    {errors.task && <span>This field is required</span>}
+                    <input className=" bg-slate-100 rounded-md p-4 m-4"
+                        type="text"
+                        placeholder="Create a new todo"
+                        {...register("task", { required: true })}
+                    />
+                </div>
+
+                <button
+                    className='  bg-blue-600 font-bold px-6 py-2 rounded-full'
+                >
+                    Add Task
+                </button>
+            </form>
         </div >
     )
 }
